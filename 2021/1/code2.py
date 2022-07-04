aef0bd5830e2f6f43b7c76c5773e1a9a86f14dd9
user <user@hostname.localdomain>
2022-07-04 23:38:52 +0300

    solved 2021/1

---
+++ 2021/1/code2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 2021/1/code2.py
+
+
+if __name__ == '__main__':
+    with open('input.txt', 'r') as f:
+        file = f.read().split('\n')
+        file.pop(len(file) - 1)
+        file = list(map(int, file))
+        counter = 0
+        for i in range(1, len(file) - 2):
+            if (file[i] + file[i + 1] + file[i + 2] >
+                    file[i - 1] + file[i] + file[i + 1]):
+                counter += 1
+        print(counter)
