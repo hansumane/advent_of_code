11c5264ac5f3d217b186e730ab65164616e2e765
Creasure <yurxo@bk.ru>
2022-12-04 02:02:27 +0300

    added answers to 2022/02

--- 2022/03/main.py
+++ 2022/03/main.py
@@ -1,3 +1,10 @@
+# correct answers:
+# 1: 7826
+# 2: 2577
+
+
+alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
 with open('input.txt', 'r') as f:
     lines = [line.strip() for line in f.readlines()]
 
@@ -6,7 +13,7 @@ res = 0
 for line in lines:
     for symbol in line[:len(line) // 2]:
         if symbol in line[len(line) // 2:]:
-            res += 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.index(symbol) + 1
+            res += alphabet.index(symbol) + 1
             break
 print(res)
 
@@ -15,6 +22,6 @@ res = 0
 for group in [lines[i:i + 3] for i in range(0, len(lines), 3)]:
     for symbol in group[0]:
         if symbol in group[1] and symbol in group[2]:
-            res += 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.index(symbol) + 1
+            res += alphabet.index(symbol) + 1
             break
 print(res)
