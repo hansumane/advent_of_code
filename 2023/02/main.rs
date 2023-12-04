d82b40482bf0c1e94680042f25d7f91f57aac813
Creasure <yurxo@bk.ru>
2023-12-05 01:15:03 +0300

    2023: 02: replaced declaration type with turbofish syntax

--- 2023/02/main.rs
+++ 2023/02/main.rs
@@ -7,7 +7,7 @@ use std::{
 
 fn main() {
     let br = BufReader::new(File::open(args().collect::<Vec<_>>().get(1).unwrap()).unwrap());
-    let max_values: HashMap<&str, u64> = HashMap::from([("red", 12), ("green", 13), ("blue", 14)]);
+    let max_values = HashMap::from([("red", 12), ("green", 13), ("blue", 14)]);
 
     let mut res1 = 0u64;
     let mut res2 = 0u64;
@@ -20,8 +20,8 @@ fn main() {
                 'first: for game in line.split(": ").last().unwrap().split("; ") {
                     for cubes in game.split(", ") {
                         let mut cubsplit = cubes.split(' ');
-                        let (amount, color): (u64, &str) = (
-                            cubsplit.next().unwrap().parse().unwrap(),
+                        let (amount, color) = (
+                            cubsplit.next().unwrap().parse::<u64>().unwrap(),
                             cubsplit.next().unwrap(),
                         );
                         if amount > max_values[color] {
@@ -35,13 +35,13 @@ fn main() {
                     res1 += li as u64 + 1;
                 }
 
-                let mut mins: HashMap<&str, u64> = HashMap::from([("red", 0), ("green", 0), ("blue", 0)]);
+                let mut mins = HashMap::from([("red", 0), ("green", 0), ("blue", 0)]);
 
                 for game in line.split(": ").last().unwrap().split("; ") {
                     for cubes in game.split(", ") {
                         let mut cubsplit = cubes.split(' ');
-                        let (amount, color): (u64, &str) = (
-                            cubsplit.next().unwrap().parse().unwrap(),
+                        let (amount, color) = (
+                            cubsplit.next().unwrap().parse::<u64>().unwrap(),
                             cubsplit.next().unwrap(),
                         );
                         if mins[color] < amount {
