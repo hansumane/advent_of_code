137039dab0a220bc1daad953fad45a7b8dbcbdd9
Creasure <yurxo@bk.ru>
2023-12-09 00:18:47 +0300

    2023: 07: solved in Rust

--- 2023/07/main.rs
+++ 2023/07/main.rs
@@ -21,20 +21,32 @@ enum HandType {
 struct Hand {
     hand: String,
     htype: HandType,
+    jokers: bool,
 }
 
 impl Hand {
-    fn new(s: &str) -> Self {
+    fn new(s: &str, jokers: bool) -> Self {
         let hand = s.to_owned();
 
         let mut temp = s.chars().collect::<Vec<_>>();
-        temp.sort_by(|c1, c2| get_card_strength(c2).partial_cmp(&get_card_strength(c1)).unwrap());
-        let temp = temp.iter().map(|v| v.to_string()).collect::<Vec<_>>().join("");
-        let htype = get_hand_type(&temp);
+        temp.sort_by(|c1, c2| {
+            get_card_strength(c2, jokers)
+                .partial_cmp(&get_card_strength(c1, jokers))
+                .unwrap()
+        });
+
+        let temp = temp
+            .iter()
+            .map(|v| v.to_string())
+            .collect::<Vec<_>>()
+            .join("");
+
+        let htype = get_hand_type(&temp, jokers);
 
         Hand {
             hand,
             htype,
+            jokers,
         }
     }
 }
@@ -52,7 +64,9 @@ impl PartialOrd for Hand {
             Some(temp)
         } else {
             for (c1, c2) in self.hand.chars().zip(other.hand.chars()) {
-                let temp = get_card_strength(&c1).partial_cmp(&get_card_strength(&c2)).unwrap();
+                let temp = get_card_strength(&c1, self.jokers)
+                    .partial_cmp(&get_card_strength(&c2, self.jokers))
+                    .unwrap();
                 if temp != Ordering::Equal {
                     return Some(temp);
                 }
@@ -62,12 +76,15 @@ impl PartialOrd for Hand {
     }
 }
 
-fn get_card_strength(c: &char) -> i64 {
+fn get_card_strength(c: &char, jokers: bool) -> i64 {
     match *c {
         'A' => 14,
         'K' => 13,
         'Q' => 12,
-        'J' => 11,
+        'J' => match jokers {
+            true => 1,
+            false => 11,
+        },
         'T' => 10,
         '9' => 9,
         '8' => 8,
@@ -81,14 +98,28 @@ fn get_card_strength(c: &char) -> i64 {
     }
 }
 
-fn get_hand_type(s: &str) -> HandType {
+fn get_hand_type(s: &str, jokers: bool) -> HandType {
     let mut amounts = HashMap::<char, usize>::new();
     for c in s.chars() {
         amounts.insert(c, s.matches(c).count());
     }
 
+    let mut adder = 0usize;
+    if jokers && amounts.get(&'J').is_some() {
+        adder += amounts.get(&'J').unwrap();
+        amounts.remove(&'J');
+    }
+
     let mut amounts = amounts.into_iter().collect::<Vec<_>>();
     amounts.sort_by(|(.., c1), (.., c2)| c2.partial_cmp(c1).unwrap());
+
+    if jokers {
+        if amounts.is_empty() {
+            amounts.push(('A', adder));
+        } else {
+            amounts[0].1 += adder;
+        }
+    }
 
     let mut amounts = amounts.into_iter();
     let (.., c1) = amounts.next().unwrap();
@@ -125,20 +156,33 @@ fn main() {
         .collect::<Vec<_>>();
 
     let mut sets = Vec::<(Hand, i64)>::with_capacity(lines.len());
-    for line in lines {
+    for line in &lines {
         let mut l = line.split_whitespace();
-        let h = Hand::new(l.next().unwrap());
+        let h = Hand::new(l.next().unwrap(), false);
         let a = l.next().unwrap().parse::<i64>().unwrap();
         sets.push((h, a));
     }
 
+    let mut sets_jokers = Vec::<(Hand, i64)>::with_capacity(lines.len());
+    for line in &lines {
+        let mut l = line.split_whitespace();
+        let h = Hand::new(l.next().unwrap(), true);
+        let a = l.next().unwrap().parse::<i64>().unwrap();
+        sets_jokers.push((h, a));
+    }
+
     let mut res1 = 0i64;
-
     sets.sort_by(|(h1, ..), (h2, ..)| h1.partial_cmp(h2).unwrap());
-
     for (i, (.., a)) in sets.iter().enumerate() {
         res1 += (i as i64 + 1) * *a;
     }
 
+    let mut res2 = 0i64;
+    sets_jokers.sort_by(|(h1, ..), (h2, ..)| h1.partial_cmp(h2).unwrap());
+    for (i, (.., a)) in sets_jokers.iter().enumerate() {
+        res2 += (i as i64 + 1) * *a;
+    }
+
     println!("1) {res1}");
+    println!("2) {res2}");
 }
